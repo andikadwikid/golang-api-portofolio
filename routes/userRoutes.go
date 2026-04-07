7303c0e8350cc9a21094b1dd18ac1940862ee447
andika.dwiki <andika0106dwiki@gmail.com>
2026-04-07 09:44:07 +0700

    feat(user): add test endpoint for debugging
    Add a simple /users/test endpoint that returns a JSON message to help with
    debugging and verifying that the user routes are correctly set up.

--- routes/userRoutes.go
+++ routes/userRoutes.go
@@ -1,6 +1,8 @@
 package routes
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 
 	"portofolio-api/controllers"
@@ -10,6 +12,9 @@ import (
 func UserRoutes(r *gin.Engine) {
 	users := r.Group("/users")
 	{
+		users.GET("/test", func(c *gin.Context) {
+			c.JSON(http.StatusOK, gin.H{"message": "Hello, World!"})
+		})
 		users.GET("/", middlewares.AuthMiddleware(), controllers.GetUsers)
 		users.POST("/register", controllers.RegisterUser)
 		users.POST("/login", controllers.LoginUser)
